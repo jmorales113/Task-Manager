1eb77d7c62b68d7a78454a002cd9cc5035a51f7d
jmorales113 <jmorales113@gmail.com>
2020-12-13 17:27:26 -0800

    Passed test for fetching user tasks

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,30 +1,15 @@
 const request = require("supertest")
-const jwt = require("jsonwebtoken")
-const mongoose = require("mongoose")
 const app = require("../src/app")
 const User = require("../src/models/user.js")
+const { userOneId, userOne, setupDatabase } = require("./fixtures/db.js")
 
-const userOneId = new mongoose.Types.ObjectId()
-const userOne = {
-    _id: userOneId,
-    name: "Brian",
-    email: "brian@example.com",
-    password: "56what!!",
-    tokens: [{
-        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
-    }]
-}
-
-beforeEach(async () => {
-    await User.deleteMany()
-    await new User(userOne).save()
-})
+beforeEach(setupDatabase)
 
 test("Should signup a new user", async () => {
     const response = await request(app).post("/users").send({
         name: "Andrew",
         email: "andrew@example.com",
-        password: "MyPass777!"
+        password: "MyPass777!" 
     }).expect(201)
 
     const user = await User.findById(response.body.user._id)
